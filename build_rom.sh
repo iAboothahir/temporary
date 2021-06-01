17494b20d7e2654778a343bb819d0ee2e3ba2caf
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-01 23:36:00 +0530

    Added patches

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b StatiX .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patches
-#cd external/selinux
-#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-#patch -p1 < *.patch
-#cd ../..
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
 
 # build rom 
 source build/envsetup.sh
