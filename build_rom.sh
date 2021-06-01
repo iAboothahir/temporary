4c5b03c40f50d155cdc0036d6247ad4076c9357e
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-01 21:35:03 +0530

    [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init -u git://github.com/AospExtended/manifest.git -b 11.x --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b aex .repo/local_manifests
+repo init -u https://github.com/StatiXOS/android_manifest.git -b 11 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b StatiX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patches
-cd external/selinux
-curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-patch -p1 < *.patch
-cd ../..
+#cd external/selinux
+#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+#patch -p1 < *.patch
+#cd ../..
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX1941-userdebug  
-m aex
+lunch statix_RMX1941-userdebug  
+brunch statix_RMX1941-userdebug
 
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
