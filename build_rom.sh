f0ebe476f600fd901e5514bc9cc54e7315067f43
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-05 15:03:56 +0530

    TenX

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/TenX-OS/manifest_TenX.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b TenX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patches
-cd external/selinux
-curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-patch -p1 < *.patch
-cd ../..
+#cd external/selinux
+#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+#patch -p1 < *.patch
+#cd ../..
 
 # build rom
 source build/envsetup.sh
-lunch evolution_RMX1941-userdebug
+lunch aosp_RMX1941-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+brunch RMX1941
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
